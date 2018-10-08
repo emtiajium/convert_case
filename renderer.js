a69e0ded2e77eec60204395daf8386ea2ca3e044
emtiajium <271emtiaj@gmail.com>
2018-10-08 13:02:44 +0600

    working

--- renderer.js
+++ renderer.js
@@ -1,3 +1,40 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+/**
+ * Created by emtiaj on 10/8/18
+ */
+
+const $ = require('jquery');
+const _string = require('underscore.string');
+const autoSize = require('autosize');
+
+$(document).ready(() => {
+	let textSelector = $('#main-text');
+	autoSize(textSelector);
+
+	$('#lower-case').on('click', () => {
+		textSelector.val(textSelector.val().toLowerCase());
+	});
+
+	$('#upper-case').on('click', () => {
+		textSelector.val(textSelector.val().toUpperCase());
+	});
+
+	$('#capitalized-case').on('click', () => {
+		textSelector.val(_string.titleize(textSelector.val()));
+	});
+
+	$('#alternating-case').on('click', () => {
+		textSelector.val(_string.swapCase(textSelector.val()));
+	});
+
+	$('#title-case').on('click', () => {
+		textSelector.val(_string.titleize(textSelector.val()));
+	});
+
+	$('#inverse-case').on('click', () => {
+		textSelector.val(_string.swapCase(textSelector.val()));
+	});
+
+	$('#clear-text').on('click', () => {
+		textSelector.val('');
+	});
+});
